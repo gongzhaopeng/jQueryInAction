0f1b880cc5f35160606046833a6cfd9d428fd09d
gongzhaopeng <gongguochuanqi@gmail.com>
2016-02-03 18:05:13 +0800

    modified:   app/controllers/BootCloset.scala 	modified:   conf/routes 	new file:   public/temphtml/c10/phase.2.html

--- app/controllers/BootCloset.scala
+++ app/controllers/BootCloset.scala
@@ -2,13 +2,21 @@ package controllers
 
 import play.api.mvc._
 import services.Boot
+import play.api.data._
+import play.api.data.Forms._
 
 /**
  * Created by mare on 16/2/3.
  */
 class BootCloset extends Controller {
 
-  def bootDetails(model: String) = Action {
+  val detailForm = Form (
+    single("model" -> nonEmptyText)
+  )
+
+  def bootDetail = Action { implicit request =>
+
+    val model = detailForm.bindFromRequest.get
 
     val boot = Boot.find(model)
     Ok(views.html.c10.bootDetails(boot))
